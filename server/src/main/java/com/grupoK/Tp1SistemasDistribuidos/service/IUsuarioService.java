a1c9ee966e9f398db1b3bf76873fa957128c0923
tomasriv2017 <tomyriv@hotmail.com>
2025-09-11 15:46:48 -0300

    Se agrega servicio grpc para obtener un unico usuario

--- server/src/main/java/com/grupoK/Tp1SistemasDistribuidos/service/IUsuarioService.java
+++ server/src/main/java/com/grupoK/Tp1SistemasDistribuidos/service/IUsuarioService.java
@@ -7,5 +7,6 @@ import com.grupoK.Tp1SistemasDistribuidos.entities.Usuario;
 public interface IUsuarioService {
 	
 	List<Usuario> findAll();
+	Usuario findById(Integer id);
 	Usuario saveOrUpdate(Usuario usuario);
 }
