eec93603b7792722f846c1e735568373df14cef4
PriscilaReinoso <reinosopriscila.pr@gmail.com>
2025-10-12 11:39:36 -0300

    correcion proto

---
+++ grpc-server/src/main/java/com/grupoK/grpc/server/wrappers/SolicitudWrapper.java
@@ -0,0 +1,17 @@
+package com.grupoK.grpc.server.wrappers;
+
+import java.util.List;
+
+import com.grupoK.connector.database.entities.Solicitud;
+import com.grupoK.connector.database.entities.SolicitudDonacion;
+
+public class SolicitudWrapper {
+    /*public com.grupoK.grpc.proto.Rol toGrpcRol(Solicitud solicitud, List<SolicitudDonacion> donaciones) {
+        return com.grupoK.grpc.proto.Solicitud.newBuilder().
+                .setId(rolModel.getId())
+                .setDescripcion(rolModel.getRol().getDescription())
+                .build();
+    }*/
+    
+
+}
