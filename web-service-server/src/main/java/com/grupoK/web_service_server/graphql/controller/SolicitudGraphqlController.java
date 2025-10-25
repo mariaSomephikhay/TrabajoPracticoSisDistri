48808832ca940d5f1cf4241fcbc85d2a940ea490
PriscilaReinoso <reinosopriscila.pr@gmail.com>
2025-10-25 15:54:07 -0300

    agrego llamado graphql con informe de donaciones

--- web-service-server/src/main/java/com/grupoK/web_service_server/graphql/controller/SolicitudGraphqlController.java
+++ web-service-server/src/main/java/com/grupoK/web_service_server/graphql/controller/SolicitudGraphqlController.java
@@ -2,33 +2,24 @@ package com.grupoK.web_service_server.graphql.controller;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
-import com.grupoK.connector.database.entities.Categoria;
 import com.grupoK.connector.database.entities.Donacion;
-import com.grupoK.connector.database.entities.Evento;
 import com.grupoK.connector.database.entities.Solicitud;
 import com.grupoK.connector.database.entities.SolicitudDonacion;
 import com.grupoK.connector.database.entities.enums.TipoCategoria;
 import com.grupoK.connector.database.repositories.ICategoriaRepository;
 import com.grupoK.connector.database.serviceImp.SolicitudService;
-import com.grupoK.web_service_server.graphql.model.SolicitudDonacionGraphqlDto;
-
 
 import com.grupoK.web_service_server.graphql.model.FiltroSolicitudInput;
 import com.grupoK.web_service_server.graphql.model.GraphQLResponse;
-import com.grupoK.web_service_server.graphql.model.InformeEventoDto;
 import com.grupoK.web_service_server.graphql.model.InformeSolicitudDto;
 
 @Controller
@@ -40,7 +31,7 @@ public class SolicitudGraphqlController {
 	@Autowired
 	ICategoriaRepository categoriaRepository;	
 	
-	@QueryMapping
+	/*@QueryMapping
 	public SolicitudDonacionGraphqlDto getSolicitud(@Argument String id) {
 		try {
 			Solicitud solicitud = solicitudService.findById(id);
@@ -55,10 +46,10 @@ public class SolicitudGraphqlController {
 		}catch (Exception e){
 			return null;
 		}
-	}
+	}*/
 	
 	@QueryMapping
-	public List<InformeSolicitudDto> informeSolicitudes(@Argument FiltroSolicitudInput filtro) {
+	public GraphQLResponse<List<InformeSolicitudDto>> informeSolicitudes(@Argument FiltroSolicitudInput filtro) {
 		LocalDateTime fechaDesdeDate = filtro.getFechaHasta() != null ? LocalDate.parse(filtro.getFechaHasta()).atStartOfDay()  : null;
 		LocalDateTime fechaHastaDate = filtro.getFechaHasta() != null ? LocalDate.parse(filtro.getFechaHasta()).atStartOfDay() : null;
 	    Boolean eliminado;
@@ -69,16 +60,8 @@ public class SolicitudGraphqlController {
 	    }else {
 	    	eliminado = false;
 	    }
-	   /* Categoria c;
-	    System.out.println(filtro.getCategoria().getDescription());
-	    if (filtro.getCategoria()!=null && !filtro.getCategoria().getDescription().equals("")) {
-	    	System.out.println(filtro.getCategoria().getDescription());
-	    	c = categoriaRepository.findByDescripcion(filtro.getCategoria().getDescription()).get();
-	    	System.out.println("a");
-	    }else {
-	    	c = null;
-	    }*/
-	    System.out.println(filtro.getCategoria());
+	   System.out.println(eliminado);
+	    
 	    List<Solicitud> solicitudes = solicitudService.find(filtro.getCategoria(), fechaDesdeDate, fechaHastaDate, eliminado);
 	    
 	    
@@ -88,11 +71,14 @@ public class SolicitudGraphqlController {
 			List<SolicitudDonacion> donationsAssociated = solicitudService.findAllDonationsAssociatedByRequest(solicitud);
 			List<Donacion> donaciones = donationsAssociated.stream()
 	                .map(SolicitudDonacion::getDonacion)
+	                .filter(don -> filtro.getCategoria() == null || 
+	                        filtro.getCategoria().equals(don.getCategoria().getDescripcion()))
 	                .toList();
+	                
 			String eliminadoINF = solicitud.getActiva() ? "NO" : "SI";
 			Boolean recibida = solicitud.getOrganizacionSolicitante().getId()!=1;
 			for (Donacion d : donaciones) {
-			System.out.println(solicitud.getId());
+			
 				TipoCategoria categoria = d.getCategoria().getDescripcion();
 				
 				Optional<InformeSolicitudDto> encontrada = informes.stream()
@@ -110,13 +96,15 @@ public class SolicitudGraphqlController {
 				}
 			}
 			}catch (Exception e) {
-				System.out.println("Solicitud sin donaciones: " + e);
+				System.out.println("Solicitud sin donaciones: " + e.getMessage());
+				return new GraphQLResponse<>("fail", e.getMessage(), null);
 			}
 			
 		}
 		System.out.println(solicitudes);
 		System.out.println(informes);
-		return informes;
+		//return informes;
+		return new GraphQLResponse<>("success","Informe obtenido", informes);
 	}
 	
 
